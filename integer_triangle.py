96e9d0278919699404cad8f26608cc04105a16a5
Kwangwon Song <48322490+remaindere@users.noreply.github.com>
2021-03-09 22:45:46 +0900

    Update integer_triangle.py

--- integer_triangle.py
+++ integer_triangle.py
@@ -13,5 +13,5 @@ def solution(triangle):
     answer = max(triangle[len(triangle)-1])
     return answer
 
-#https://programmers.co.kr/learn/courses/30/lessons/42576
+#https://programmers.co.kr/learn/courses/30/lessons/43105
 #출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
