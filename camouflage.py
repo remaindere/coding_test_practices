3404f207a7b27f94ecce103a8cd14f0936bea06a
Kwangwon Song <48322490+remaindere@users.noreply.github.com>
2021-03-09 22:46:32 +0900

    Update camouflage.py

--- camouflage.py
+++ camouflage.py
@@ -15,5 +15,5 @@ def solution(clothes):
     answer = mul_v -1
     return answer
 
-#https://programmers.co.kr/learn/courses/30/lessons/42576
+#https://programmers.co.kr/learn/courses/30/lessons/42578
 #출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
