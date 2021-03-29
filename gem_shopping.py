f4a3c234a47058e5d0f920b0038789f9176df621
Kwangwon Song <48322490+remaindere@users.noreply.github.com>
2021-03-29 21:34:56 +0900

    Update gem_shopping.py

--- gem_shopping.py
+++ gem_shopping.py
@@ -6,11 +6,11 @@ def solution(gems):
     l_opt = 0
     r_opt = len(gems)-1
     
-    yet = 0
+    yet = 0 # 탈출조건
     for gem in gems:
         gems_dict[gem] = gems_dict.get(gem,0)
     
-    while l_cur < len(gems)-1 and r_cur < len(gems)-1 and l_cur<=r_cur:
+    while l_cur < len(gems) and r_cur < len(gems) and l_cur<=r_cur:
         gems_dict[gems[r_cur]]+=1
         yet = 0
         
@@ -19,13 +19,13 @@ def solution(gems):
                 yet=1
         
         if(yet):
-            r_cur+=1
+            r_cur+=1 #오른쪽 커서 이동 (탐색범위 증가)
         else:
             if(r_opt-l_opt>r_cur-l_cur):
                 l_opt = l_cur   
                 r_opt = r_cur
             gems_dict[gems[l_cur]]-=1
-            l_cur+=1
+            l_cur+=1 #왼쪽 커서 이동 (탐색범위 감소)
     
     answer = [l_opt+1, r_opt+1]
     return answer
