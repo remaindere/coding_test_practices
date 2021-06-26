6771b878c06b8c74011a1f6613afa2a7b293c9ca
Kwangwon Song <48322490+remaindere@users.noreply.github.com>
2021-06-27 00:05:38 +0900

    visibility improved

--- sugar_delivery.py
+++ sugar_delivery.py
@@ -1,12 +1,16 @@
 N = int(input())
+
 sugar = 0
+
 while N >= 0 : # while sugar bags are exist
   if N % 5 == 0 :
     sugar += N // 5
     break
-  N = N - 3
+    
+  N -= 3
   sugar += 1
-else : # N < 0, it can't be divided by zero
+  
+if N < 0 : # it can't be divided by zero
   print(-1)
-print(sugar)
-# beakjoon, https://www.acmicpc.net/problem/2839
+else : # div process ended successful
+  print(sugar)
